872380ff1a05e1641454ee49a822a58d734df70e
raakinrashid-max <raakinrashid@gmail.com>
2025-09-03 19:53:19 -0400

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+
+int main (){
+	
+	int scores[5] = {90,85,70,95,100};
+	int sum =0;
+	for(int i=0;i<5;i++){
+	sum+=scores[i];
+	
+	
+	
+	}
+	std::cout<<"average"<<sum/5<<"/n";
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
